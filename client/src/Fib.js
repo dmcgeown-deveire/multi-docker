61b40508d9668dc9a00b1dc7da43ed9213c0cebd
dmcgeown <dmcgeown@acquisbi.com>
2021-06-04 16:09:23 +0100

    Removing deploy step from build file.zdfsadf

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -16,13 +16,13 @@ class Fib extends Component {
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
-    this.setState({values: values.data});
+    this.setState({values: values.status === 200 ? values.data : {}});
   }
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({
-      seenIndexes: seenIndexes.data.number ? seenIndexes.data : [],
+      seenIndexes: seenIndexes.status === 200 ? seenIndexes.data : [],
     });
   }
 
